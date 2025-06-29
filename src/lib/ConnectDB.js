6fcf878c443d49190f125903151648c975345a2a
Utsab Adhikari <utsabadhikari075@gmail.com>
2025-06-29 20:36:40 +0545

    updated

---
+++ src/lib/ConnectDB.js
@@ -0,0 +1,15 @@
+
+import mongoose from "mongoose";
+
+const ConnectDB = async () => {
+    try {
+        
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Database Connected");
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default ConnectDB;
