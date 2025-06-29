c7159b55e646d5b6675fe8ca4a6d201e08875cbc
Utsab Adhikari <utsabadhikari075@gmail.com>
2025-06-29 20:46:32 +0545

    updated

--- src/lib/ConnectDB.js
+++ src/lib/ConnectDB.js
@@ -2,14 +2,10 @@ import mongoose from "mongoose";
 
 const ConnectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("✅ Database Connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database Connected");
   } catch (error) {
-    console.error("❌ Database connection error:", error);
-    throw new Error("Database connection failed");
+    console.log(error);
   }
 };
 
